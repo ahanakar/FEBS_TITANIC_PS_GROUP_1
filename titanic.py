ff70c7aaa8a5a4848f8f4f479aecc3650e37583a
adityayadav0206 <yadavaditya888888@gmail.com>
2025-12-14 15:14:14 +0530

    started working

--- titanic.py
+++ titanic.py
@@ -1,9 +1,22 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as pl
-import seaborn as sns
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
 
-train_df=pd.read_csv('train.csv')
-test_df=pd.read_csv('test.csv')
+def feature_engineering(train):
+    train['Group'] = train['PassengerId'].str.split('_').str[0]
+ 
+    train['GroupSize'] = train.groupby('Group')['Group'].transform('count')
+    
+    train[['Deck', 'CabinNumber', 'Side']] = train['Cabin'].str.split('/', expand=True)
+    
+    train['CabinNumber'] = pd.to_numeric(train['CabinNumber'], errors='coerce')
+    
+    spend_cols = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
+    
+    train[spend_cols] = train[spend_cols].fillna(0)
 
-print(train_df.head())
+    train['TotalSpend'] = train[spend_cols].sum(axis=1)
+    return train
+train_fe = feature_engineering(train)
+print(train_fe)
